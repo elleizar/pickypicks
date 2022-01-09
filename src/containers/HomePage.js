54da52d6cae33fa12be1b7cd7060231dedd34a9d
Thalia <thaliag@my.yorku.ca>
2022-01-08 19:47:01 -0500

    Added fonts, images, and draft home page

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,14 +1,22 @@
 import { Component  } from "react";
 import { Link } from 'react-router-dom';
+import background from '../images/background.png';
+import '../css/Home.css';
 
 export class HomePage extends Component {
   render() {
     return (
       <div className="HomePage">
         <h1 className="home-header">
-          pickypicks
+          PICKY PICKS
         </h1>
-        <img src="" className="home-emoji" alt="emoji" />
+        <h2 className="home-subheader">
+          for picky eaters.
+        </h2>
+        <img src={background} className="home-bg" alt="bg" />
+        <p className="body-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum sem eget molestie lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean felis orci, auctor in faucibus vitae, fringilla nec ex. In non tellus non leo finibus fermentum ut a ipsum. Duis a diam sed ex feugiat fermentum.
+        </p>
         <br/>
         <Link to='/recipes'>
           <button 
